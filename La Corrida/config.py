6eddceaa880c214f87caf896b277ebf89738b829
Martin Rios-Cardenas <59406376+themexpride@users.noreply.github.com>
2021-02-21 09:34:50 -0800

    Update config.py

--- La Corrida/config.py
+++ La Corrida/config.py
@@ -9,4 +9,4 @@ class DefaultConfig:
 
     PORT = 3978
     APP_ID = os.environ.get("MicrosoftAppId", "e0571edf-341f-451e-960e-eeb340116da9")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "953ebab9-77c7-4760-9b1c-67352ed17c59")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "nR21v5DT^w:gEVJ}fx}(Um6LX)
