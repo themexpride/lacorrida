fc5d92537f4ebfdf9a708305a4f83af1c66ec07b
Martin Rios-Cardenas <59406376+themexpride@users.noreply.github.com>
2021-02-21 02:25:05 -0800

    Update config.py

--- La Corrida/config.py
+++ La Corrida/config.py
@@ -8,5 +8,5 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_ID = os.environ.get("MicrosoftAppId", "e0571edf-341f-451e-960e-eeb340116da9")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "953ebab9-77c7-4760-9b1c-67352ed17c59")
