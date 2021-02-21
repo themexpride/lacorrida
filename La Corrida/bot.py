6e8cd8e6bcf376f81ab3925798566783f3673c86
PC <pranavc10@hotmail.com>
2021-02-21 18:54:22 +0400

    added options to pick from

--- La Corrida/bot.py
+++ La Corrida/bot.py
@@ -1,21 +1,82 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-from botbuilder.core import ActivityHandler, TurnContext
-from botbuilder.schema import ChannelAccount
+from botbuilder.core import ActivityHandler, MessageFactory, TurnContext
+from botbuilder.schema import ChannelAccount, CardAction, ActionTypes, SuggestedActions
 
 
 class MyBot(ActivityHandler):
-    # See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+	"""
+	This bot will respond to the user's input with suggested actions.
+	Suggested actions enable your bot to present buttons that the user
+	can tap to provide input.
+	"""
 
-    async def on_message_activity(self, turn_context: TurnContext):
-        await turn_context.send_activity(f"You said '{ turn_context.activity.text }'")
+	async def on_members_added_activity(
+		self, members_added: [ChannelAccount], turn_context: TurnContext
+	):
+		"""
+		Send a welcome message to the user and tell them what actions they may perform to use this bot
+		"""
 
-    async def on_members_added_activity(
-        self,
-        members_added: ChannelAccount,
-        turn_context: TurnContext
-    ):
-        for member_added in members_added:
-            if member_added.id != turn_context.activity.recipient.id:
-                await turn_context.send_activity("Hello and welcome!")
+		return await self._send_welcome_message(turn_context)
+
+	async def on_message_activity(self, turn_context: TurnContext):
+		"""
+		Respond to the users choice and display the suggested actions again.
+		"""
+
+		text = turn_context.activity.text.lower()
+		response_text = self._process_input(text)
+
+		await turn_context.send_activity(MessageFactory.text(response_text))
+
+		return await self._send_suggested_actions(turn_context)
+
+	async def _send_welcome_message(self, turn_context: TurnContext):
+		for member in turn_context.activity.members_added:
+			if member.id != turn_context.activity.recipient.id:
+				await turn_context.send_activity(
+					MessageFactory.text(
+						"Hello and Welcome!"
+					)
+				)
+
+				await self._send_suggested_actions(turn_context)
+
+	def _process_input(self, text: str):
+
+		if text == "Housing":
+			return f"do Housing"
+
+		if text == "Resources":
+			return f"do Resources"
+
+		return "Please select a valid option from the provided choices"
+
+	async def _send_suggested_actions(self, turn_context: TurnContext):
+		"""
+		Creates and sends an activity with suggested actions to the user. When the user
+		clicks one of the buttons the text value from the "CardAction" will be displayed
+		in the channel just as if the user entered the text. There are multiple
+		"ActionTypes" that may be used for different situations.
+		"""
+
+		reply = MessageFactory.text("What are you interested in?")
+
+		reply.suggested_actions = SuggestedActions(
+			actions=[
+				CardAction(
+					title="Housing",
+					type=ActionTypes.im_back,
+					value="Housing",
+				),
+				CardAction(
+					title="Resources",
+					type=ActionTypes.im_back,
+					value="Resources",
+				),
+			]
+		)
+
+		return await turn_context.send_activity(reply)
